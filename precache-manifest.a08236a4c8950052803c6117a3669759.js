a70ba08773ac7617f662b3f720cdad0c39b187e8
jcaviedes <jcaviedes@superfuds.com.co>
2020-07-25 15:00:10 -0500

    Updates

---
+++ precache-manifest.a08236a4c8950052803c6117a3669759.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70e84d5b1085a0de3746bf2593ed9de",
+    "url": "/bmwecom/index.html"
+  },
+  {
+    "revision": "c462bbc12adc2d458583",
+    "url": "/bmwecom/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "f66b8bb70e4dbf3ece9d",
+    "url": "/bmwecom/static/css/main.7c7489a5.chunk.css"
+  },
+  {
+    "revision": "c462bbc12adc2d458583",
+    "url": "/bmwecom/static/js/2.007dbeba.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/bmwecom/static/js/2.007dbeba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66b8bb70e4dbf3ece9d",
+    "url": "/bmwecom/static/js/main.559b302c.chunk.js"
+  },
+  {
+    "revision": "f218a9a3ea239de132c1",
+    "url": "/bmwecom/static/js/runtime-main.edc6b36b.js"
+  },
+  {
+    "revision": "3a67111c4d59a957393eb69f4e203434",
+    "url": "/bmwecom/static/media/cart.3a67111c.svg"
+  },
+  {
+    "revision": "295bc3ddc236c8b9f64cc4ff2828f7f7",
+    "url": "/bmwecom/static/media/check.295bc3dd.svg"
+  },
+  {
+    "revision": "a4902a7982d311ae296a6478ed2639fd",
+    "url": "/bmwecom/static/media/search.a4902a79.svg"
+  },
+  {
+    "revision": "72b6e79467b03a8f9f0812fe5b818a8f",
+    "url": "/bmwecom/static/media/side-menu.72b6e794.svg"
+  },
+  {
+    "revision": "fe7c892b27e8347be28196143f69bafa",
+    "url": "/bmwecom/static/media/user.fe7c892b.svg"
+  }
+]);
